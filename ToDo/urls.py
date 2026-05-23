8cd468597a2c73459c4a0b042d6ce3044f4ed5f0
SYN 606 <postbox.501@yandex.com>
2026-05-23 12:41:16 +0530

    UPDATE: Major update in templates

--- ToDo/urls.py
+++ ToDo/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path("ckeditor5/", include('django_ckeditor_5.urls')),
 ]
 
-# Serve static and media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
